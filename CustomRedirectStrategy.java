3c292e7934551d9feefdc054a22b7e28c53499b1
ptomjie <424222352@qq.com>
2023-02-02 14:15:55 +0800

    add sh

---
+++ CustomRedirectStrategy.java
@@ -0,0 +1,44 @@
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestWrapper;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.protocol.HttpContext;
+import org.apache.log4j.Logger;
+
+import java.net.URI;
+
+/**
+ * @author tangzhijie
+ * @date 2019/8/19
+ * @email tangzhijie@sinotrans.com
+ * @description
+ * @since 2019/8/19
+ */
+public class CustomRedirectStrategy extends LaxRedirectStrategy {
+
+    protected static final Logger logger = Logger.getLogger(CustomRedirectStrategy.class);
+
+    @Override
+    public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context) throws ProtocolException {
+        URI uri = getLocationURI(request, response, context);
+        String method = request.getRequestLine().getMethod();
+        if ("post".equalsIgnoreCase(method)) {
+            try {
+                HttpRequestWrapper httpRequestWrapper = (HttpRequestWrapper) request;
+                httpRequestWrapper.setURI(uri);
+                httpRequestWrapper.removeHeaders("Content-Length");
+                return httpRequestWrapper;
+            } catch (Exception e) {
+                logger.error("强转为HttpRequestWrapper出错");
+            }
+            return new HttpPost(uri);
+        } else {
+            return new HttpGet(uri);
+        }
+    }
+}
